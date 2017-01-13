d30159854e278dd8507db3fba55e34ebdc6a9efe
Victor van Poppelen <vvanpo@gmail.com>
2017-01-12 19:37:51 -0800

    site/: added fonts and carousel

--- site/site.go
+++ site/site.go
@@ -6,21 +6,27 @@ import (
     "net/http"
 )
 
-var templates = template.Must(template.ParseGlob("site/templates/*"))
+var tmpl = template.Must(template.ParseGlob("site/templates/*"))
 
 type page struct {
 }
 
 func rootHandler (w http.ResponseWriter, r *http.Request) {
     if r.URL.Path == "/" {
-        p := page{}
-        templates.Execute(w, p)
+        tmpl.Execute(w, page{
+        })
     } else {
         http.FileServer(http.Dir("site/static/")).ServeHTTP(w, r)
     }
 }
 
+func memberHandler (w http.ResponseWriter, r *http.Request) {
+    tmpl.Execute(w, page{
+    })
+}
+
 func Serve (addr string) {
     http.HandleFunc("/", rootHandler)
+    http.HandleFunc("/member", memberHandler)
     log.Panic(http.ListenAndServe(addr, nil))
 }
