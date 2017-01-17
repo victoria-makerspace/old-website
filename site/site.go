0922ddfa52f661075314853f90499c1684afe6b4
Victor van Poppelen <vvanpo@gmail.com>
2017-01-16 22:45:00 -0800

    site/: enabled sign-in form database/: added session table

--- site/site.go
+++ site/site.go
@@ -4,28 +4,47 @@ import (
     "html/template"
     "log"
     "net/http"
+    "net/url"
     "os"
+    _ "crypto/rand"
+    _ "golang.org/x/crypto/scrypt"
 )
 
 //var tmpl = template.Must(template.ParseGlob(os.Getenv("MAKERSPACE_DIR") + "/site/templates/*"))
 
 type page struct {
+    Name string
+    Title string
+}
+
+func authenticate_form (post url.Values) bool {
+    return false
 }
 
 func rootHandler (w http.ResponseWriter, r *http.Request) {
     if r.URL.Path == "/" {
+        p := page{"index", ""}
+        if r.PostFormValue("signin") == "true" && !authenticate_form(r.PostForm) {
+        }
 //        tmpl = template.Must(template.ParseGlob(os.Getenv("MAKERSPACE_DIR") + "/site/templates/*"))
         tmpl := template.Must(template.ParseFiles(os.Getenv("MAKERSPACE_DIR") + "/site/templates/main.tmpl"))
-        tmpl.Execute(w, page{
-        })
+        tmpl.Execute(w, p)
     } else {
         http.FileServer(http.Dir(os.Getenv("MAKERSPACE_DIR") + "/site/static/")).ServeHTTP(w, r)
     }
 }
 
+    // salt := make([]byte, 16)
+    // _, err := rand.Read(salt)
+    // if err != nil {}
+    // scrypt.Key([]byte(password), salt, 16384, 8, 1, 32);
+
 func memberHandler (w http.ResponseWriter, r *http.Request) {
-    //tmpl.Execute(w, page{
-    //})
+    tmpl := template.Must(template.ParseFiles(os.Getenv("MAKERSPACE_DIR") + "/site/templates/main.tmpl"))
+    tmpl.Execute(w, page{
+        "member",
+        "Dashboard",
+    })
 }
 
 func Serve (addr string) {
