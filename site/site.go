0401ebad9bdfc10a3d1176cb0ee93b08e284f181
Victor van Poppelen <vvanpo@gmail.com>
2017-02-12 13:32:37 -0800

    site: join page

--- site/site.go
+++ site/site.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"path"
+	"regexp"
 )
 
 var templates = [...]string{"main",
@@ -108,6 +109,7 @@ func (p *page) http_error(code int) {
 
 func (h *Http_server) root_handler() {
 	h.member_handler()
+	h.join_handler()
 	h.mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		p := h.new_page("index", "", w, r)
 		if r.URL.Path != "/" {
@@ -129,71 +131,48 @@ func (h *Http_server) root_handler() {
 	})
 }
 
-/*
-func (s *Http_server) data_handler() {
-	s.mux.HandleFunc("/member/data/", func(w http.ResponseWriter, r *http.Request) {
-		//http.StripPrefix("/member/data/", http.FileServer(http.Dir(s.config.Data_dir))).ServeHTTP(w, r)
-	})
-}*/
-/*
-
-func (s *Http_server) join_handler() {
+func (h *Http_server) join_handler() {
 	username_rexp := regexp.MustCompile("^[\\pL\\pN\\pM\\pP]+$")
 	name_rexp := regexp.MustCompile("^(?:[\\pL\\pN\\pM\\pP]+ ?)+$")
-	s.mux.HandleFunc("/join", func(w http.ResponseWriter, r *http.Request) {
-		p := s.new_page("join", "Join")
-		s.authenticate(w, r, &p.Member)
-		if p.Member.Authenticated() {
-			http.Error(w, http.StatusText(403), 403)
+	h.mux.HandleFunc("/join", func(w http.ResponseWriter, r *http.Request) {
+		p := h.new_page("join", "Join", w, r)
+		p.authenticate()
+		if p.Session != nil {
+			p.http_error(403)
 			return
 		}
 		q := r.URL.Query()
 		if _, ok := q["exists"]; ok {
-			rsp := "nil"
+			rsp := "true"
+			var exists bool
 			if _, ok := q["username"]; ok {
-				var n int
-				err := s.db.QueryRow("SELECT COUNT(*) FROM member WHERE username = $1", q.Get("username")).Scan(&n)
-				if err != nil {
-					log.Panic(err)
-				}
-				if n == 0 {
+				if err := p.db.QueryRow("SELECT true FROM member WHERE username = $1", q.Get("username")).Scan(&exists); err != nil {
+					if err != sql.ErrNoRows {
+						log.Panic(err)
+					}
 					rsp = "false"
-				} else {
-					rsp = "true"
 				}
 			} else if _, ok := q["email"]; ok {
-				var n int
-				err := s.db.QueryRow("SELECT COUNT(*) FROM member WHERE email = $1", q.Get("email")).Scan(&n)
-				if err != nil {
-					log.Panic(err)
-				}
-				if n == 0 {
+				if err := p.db.QueryRow("SELECT true FROM member WHERE email = $1", q.Get("email")).Scan(&exists); err != nil {
+					if err != sql.ErrNoRows {
+						log.Panic(err)
+					}
 					rsp = "false"
-				} else {
-					rsp = "true"
 				}
+			} else {
+				rsp = "nil"
 			}
 			w.Write([]byte(rsp))
 		} else if r.PostFormValue("join") == "true" {
 			username_length := len([]rune(r.PostFormValue("username")))
 			if !username_rexp.MatchString(r.PostFormValue("username")) || username_length > 20 || username_length < 3 {
 			} else if !name_rexp.MatchString(r.PostFormValue("name")) {
-			} else if s.join(r.PostFormValue("username"), r.PostFormValue("name"), r.PostFormValue("email"), r.PostFormValue("password")) {
-				s.sign_in(w, r)
+			} else if m := member.New(r.PostFormValue("username"), r.PostFormValue("name"), r.PostFormValue("email"), r.PostFormValue("password"), p.db); m != nil {
+				p.new_session(m, true)
 				w.Write([]byte("success"))
-			} else {
 			}
-			return
 		} else {
-			s.tmpl.Execute(w, p)
+			p.write_template()
 		}
 	})
 }
-
-func (s *Http_server) classes_handler() {
-	s.mux.HandleFunc("/classes", func(w http.ResponseWriter, r *http.Request) {
-		p := s.new_page("classes", "Classes")
-		s.tmpl.Execute(w, p)
-	})
-}
-*/
